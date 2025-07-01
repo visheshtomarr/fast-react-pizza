ee500141a6c2892276297a5009a5bc1b9090287f
visheshtomarr <visheshtomar7800@gmail.com>
2025-07-01 13:10:31 +0530

    Added responsiveness

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="text-sm font-semibold">Vishesh</div>;
+  return <div className="hidden text-sm font-semibold">Vishesh</div>;
 }
 
 export default UserName;

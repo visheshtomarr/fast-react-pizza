f08b59521b05600a7c7bd86c315ff03e6efc5399
visheshtomarr <visheshtomar7800@gmail.com>
2025-07-01 12:37:13 +0530

    Styled some text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
+
+      <UserName />
     </header>
   );
 }

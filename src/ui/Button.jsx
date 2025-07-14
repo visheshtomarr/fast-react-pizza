50cf7c78ef2218ebb67459afb13e4e382c5fb5f2
visheshtomarr <visheshtomar7800@gmail.com>
2025-07-14 21:34:40 +0530

    Added styling to cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,8 +5,10 @@ function Button({ children, disabled, to, type }) {
     "inline-block rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring uppercase focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    primary: base + " px-4 py-3 sm:px-6 sm:py-4 text-sm",
+    primary: base + " px-4 py-3 md:px-6 md:py-4 text-sm",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "text-sm px-4 py-2.5 md:px-6 md:py-3.5 inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-600 focus:text-stone-600 focus:bg-stone-300 focus:outline-none focus:ring uppercase focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to)

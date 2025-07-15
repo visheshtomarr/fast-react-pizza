9573b33204994ff1d8407fd32ab5efce2d6ddf34
visheshtomarr <visheshtomar7800@gmail.com>
2025-07-15 14:49:46 +0530

    Setting up User state with redux toolkit

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Vishesh</div>;
+  const username = useSelector((store) => store.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default UserName;
